e79658a9ea569cb5293cd518e87a86ce8808ec50
AuskinImmanuel <auskinimmanuel.24cs@licet.ac.in>
2024-03-16 21:38:48 +0530

    initial push with dashboad view turned on

---
+++ react-ui/src/views/maps/google-maps/Marker.js
@@ -0,0 +1,14 @@
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { compose } from 'recompose';
+import React from 'react'
+
+const MapWithAMarker = compose(
+    withScriptjs,
+    withGoogleMap
+)((props) => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 28.5274851, lng: 77.1389452 }}>
+        <Marker position={{ lat: 28.5274851, lng: 77.1389452 }} />
+    </GoogleMap>
+));
+
+export default MapWithAMarker;
