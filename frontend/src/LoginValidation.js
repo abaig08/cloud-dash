45ddc605e7b1bc6a9b3d7efbca71080485379037
Saam <saamjwesley@gmail.com>
2024-04-14 12:44:16 +0530

    frontend

---
+++ frontend/src/LoginValidation.js
@@ -0,0 +1,27 @@
+function Validation(values){
+    
+    let error = {}
+    const email_pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/;
+
+    if(values.email === "") {
+        error.email = "Email should not be empty"
+    }
+    else if(!email_pattern.test(values.email)){
+        error.email = "Email Didnt match"
+    }
+    else{
+        error.email = ""
+    }
+    if(values.password === ""){
+        error.password = "Password should not be empty"
+    }
+    else if(!password_pattern.test(values.password)){
+        error.password = "Password didnt match"
+    }else{
+        error.password = ""
+    }
+    return error;
+}
+
+export default Validation
