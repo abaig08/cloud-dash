b6848a47550addab210de02824b2e57bafae774d
Saam <saamjwesley@gmail.com>
2024-04-16 02:20:35 +0530

    home page -start

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      HOME
+    <div className=' bg-primary vh-100 p-3'>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light rounded-2">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+            <Link to="/" className='btn btn-default w-100  text-decoration-none'>Map</Link>
+            </li>
+            <li class="nav-item active">
+            <Link to="/" className='btn btn-default w-100  text-decoration-none'>Status</Link>
+            </li>
+            <li class="nav-item active">
+            <Link to="/" className='btn btn-default w-100  text-decoration-none'>Report</Link>
+            </li>
+                      
+            
+          </ul>
+        </nav>
     </div>
   )
 }
