7b13f8ba6537d26c747fd2513bd3ce4644598e1e
Saam <saamjwesley@gmail.com>
2024-04-21 18:38:09 +0530

    small changes

--- frontend/src/Stat.js
+++ frontend/src/Stat.js
@@ -5,9 +5,7 @@ import { Link } from 'react-router-dom';
 
 
 function Stat() {
-  // Define the initial map center and zoom level
-  const center = [51.505, -0.09]; // London coordinates
-  const zoomLevel = 13; // Initial zoom level
+  
 
   return (
     <div className=' bg-primary vh-100 p-3'>
@@ -27,27 +25,7 @@ function Stat() {
             </li> 
           </ul>
         </nav>
-        <div class="pt-3">
-            <div class="">
-            <h2 class="pl-3">Map Display</h2>
-            {/* MapContainer is the main container for the map */}
-                <MapContainer center={center} zoom={zoomLevel} style={{ height: '400px', width: '100%' }}>
-                    {/* Add a tile layer for map tiles */}
-                    <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    />
-
-                    {/* Add a marker at the center location */}
-                    <Marker position={center}>
-                    {/* Add a popup to the marker */}
-                    <Popup>
-                        This is London.
-                    </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
-        </div>
+        
     </div>
   );
 }
